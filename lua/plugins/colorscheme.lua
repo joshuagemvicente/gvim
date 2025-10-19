0817c8fb50660a82cc48e81835544aa50f8e668f
joshuagemvicente <joshuagemvicente6@gmail.com>
2025-10-19 17:34:18 +0800

    added something

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,23 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {
-    style = "night",
-    transparent = true,
-    styles = {
-      sidebars = "transparent",
-      floats = "transparent",
-    },
+  {
+    "zaldih/themery.nvim",
+    lazy = false,
+    config = function()
+      require("themery").setup({
+        themes = {
+          "tokyonight",
+          "catppuccin",
+          "gruvbox",
+          "kanagawa",
+          "gruber-darker",
+        },
+        -- add the config here
+      })
+    end,
   },
+  { "folke/tokyonight.nvim" },
+  { "catpuccin/nvim", name = "catppuccin" },
+  { "ellisonleao/gruvbox.nvim" },
+  { "rebelot/kanagawa.nvim" },
+  { "blazkowolf/gruber-darker.nvim" },
 }
