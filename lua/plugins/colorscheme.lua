9a8abaa2626d28716b97a9774e443d710b591d64
joshuagemvicente <joshuagemvicente6@gmail.com>
2025-11-22 15:00:17 +0800

    complete merge

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -21,3 +21,27 @@ return {
   { "rebelot/kanagawa.nvim" },
   { "blazkowolf/gruber-darker.nvim" },
 }
+
+-- return {
+--   {
+--     "zaldih/themery.nvim",
+--     lazy = false,
+--     config = function()
+--       require("themery").setup({
+--         themes = {
+--           "tokyonight",
+--           "catppuccin",
+--           "gruvbox",
+--           "kanagawa",
+--           "gruber-darker",
+--         },
+--         -- add the config here
+--       })
+--     end,
+--   },
+--   { "folke/tokyonight.nvim" },
+--   { "catpuccin/nvim", name = "catppuccin" },
+--   { "ellisonleao/gruvbox.nvim" },
+--   { "rebelot/kanagawa.nvim" },
+--   { "blazkowolf/gruber-darker.nvim" },
+-- }
---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,14 @@
+return {
+  "zbirenbaum/copilot.lua",
+  opts = {
+    suggestion = {
+      auto_trigger = true, -- keep ghost text
+      keymap = {
+        accept = "<Tab>",
+      },
+    },
+    panel = {
+      enabled = false, -- 🔥 disables Copilot popup suggestions menu
+    },
+  },
+}
